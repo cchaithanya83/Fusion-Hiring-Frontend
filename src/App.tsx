ec8e8d5953064c10343ec6c9e2323cf54cc02ff5
Chaithanya k <cchaithanya83@gmail.com>
2025-01-25 04:02:32 +0530

    update: solved build issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import CreateJobMain from "./pages/CreateJob.tsx";
+import CreateJobMain from "./pages/CreateJobMain.tsx";
 
 const App: React.FC = () => {
   return (
